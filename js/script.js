fd2b93a49e2bcdb53e75d99c7e2d3597bfd5131c
Jastin Alberca <jastinalberca77@gmail.com>
2026-02-19 11:40:52 +0900

    WSAT-M4: JavaScript interactivity added

---
+++ js/script.js
@@ -0,0 +1,6 @@
+console.log("js/script.js") 
+const submitBtn = document.getElementById("js/script.js");
+submitBtn.addEventListener("click",function(event){
+event.preventDefault();
+alert("Thank you! Your message has been received.");
+});
